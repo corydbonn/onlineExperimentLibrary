a5a59be3b31d55572e9ede4b529b549a0f213b33
Cory Bonn <corydbonn@gmail.com>
2015-07-23 22:48:04 -0400

    Fixed bug.

--- js/mouseTracker.js
+++ js/mouseTracker.js
@@ -1,26 +1,26 @@
 //collection Area is the div id string; the dataCollectionObject is the javascript object containing 3 arrays for collecting data
 
-//helper for initializing an output object for mouse tracking data
-function MouseTrackData() {
-	this.xCoord = [];
-	this.yCoord = [];
-	this.timestamp = [];
-}
-
-function mouseTrack(collectionArea,dataCollectionObject) {
+function mouseTrack(collectionArea) {
     
-	var domObject = document.getElementById(collectionArea); // select the DOM element indicated
+	//var domObject = document.getElementById(collectionArea); // select the DOM element indicated
+	var domObject = document.getElementById(collectionArea);
+	var dataCollectionObject = [];
 
 	this.begin = function() {
 		domObject.onmousemove = function(event) {
-			dataCollectionObject.xCoord.push(event.clientX); //push data to the collection arrays
-	    	dataCollectionObject.yCoord.push(event.clientY);
-	    	dataCollectionObject.timestamp.push(Date.now());
+			dataCollectionObject.push({
+				xCoord: event.clientX, //push data to the collection arrays
+				yCoord: event.clientY,
+				timestamp: Date.now()
+			});
 		}
 	}
 	this.end = function() {
 		domObject.onmousemove = undefined;
 		domObject = undefined;
+	}
+	this.returnData = function() {
+		return dataCollectionObject;
 	}
 }
 
